f5fa77ce476aa5e9b4a53db9be46cd23410df67a
Minh Dao <minh.dao.game5@gmail.com>
2026-09-09 19:44:04 +0200

    Initial website

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+
+@app.route("/car")
+def car():
+    return "Unser Auto"
+
+
+@app.route("/project")
+def project():
+    return "Unser Projekt"
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/sponsors")
+def sponsors():
+    return render_template("sponsors.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
